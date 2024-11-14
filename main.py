7b3e9a790643e0cf12c4f7d0a80721378ee150d7
JavierS <javier.santos.suarez09@gmail.com>
2024-11-14 11:38:58 +0100

    segundo Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Query, Path
+
+from models.product import Product
+
+from routers.product import router as product_router
+
+app = FastAPI()
+
+
+@app.get('/')
+
+def message():
+    return 'Hola Mundo'
+
+app.include_router(product_router)
